766b2dbf7f5196d23073ec386335e7deb6fb279a
ahmed <ahmedissaten@gmail.com>
2023-09-07 12:14:27 -0700

    fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-// main 
-int main(void) //main 
+/*
+ * main - print using puts().
+ * Return: 0
+ *
+ */
+
+#include <stdio.h> 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-/*
- * main function
+/**
+ * main :- This progrgam is for printin.
+ * Using printf().
+ * Return: vralue 0
+ *
  */
+
+#include <stdio.h>
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
