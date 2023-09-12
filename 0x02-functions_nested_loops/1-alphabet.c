f86de450c88b0856bb8c0c9f5dec591cb2e7e737
ahmed <ahmedissaten@gmail.com>
2023-09-12 08:40:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int _putchar(char);
+void print_alphabet(void)
+{
+        int i;
+        char s[] = "abcdefghijklmnopqrstuvwxyz";
+
+        for (i = 0; i < 27; i++)
+        {
+                _putchar(s[i]);
+        }
+}
 /**
  * main - print string using _ptuchar function
  *
@@ -10,15 +20,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-void print_alphabet(void)
-{
-	int i;
-	char s[] = "abcdefghijklmnopqrstuvwxyz";
-
-	for (i = 0; i < 27; i++)
-	{
-		_putchar(s[i]);
-	}
 }
