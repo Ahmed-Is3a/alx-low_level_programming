f879baebaa9a27b84f637570db55985e1f0f7810
ahmed <ahmedissaten@gmail.com>
2023-09-12 13:23:20 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,6 +24,7 @@ void jack_bauer(void)
 			{
 				_putchar('0' + i / 10);
 				_putchar('0' + i % 10);
+			}
 			_putchar(':');
 			if (j < 10)
 			{
@@ -34,6 +35,7 @@ void jack_bauer(void)
 			{
 				_putchar('0' + j / 10);
 				_putchar('0' + j % 10);
+			}
 			_putchar('\n');
 		}
 	}
