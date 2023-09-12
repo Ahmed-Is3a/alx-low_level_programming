18247984cdf9c9e8489d81ee93aabf109c3d9100
ahmed <ahmedissaten@gmail.com>
2023-09-12 13:12:40 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,36 @@
-#include <unistd.h>
 #include <stdio.h>
-
-int _putchar(char);
-/**
- * main - print string using _ptuchar function
- *
- * Return: always 0.
- */
-int main(void)
-{
-
-}
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * jack_bauer - printing alphabets.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: no value
  */
-int _putchar(char c)
+void jack_bauer(void)
 {
-	return (write(1, &c, 1));
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0' + 0);
+				_putchar('0' + i);
+			}
+			else
+				_putchar('0' + i);
+			_putchar(':');
+			if (j < 10)
+			{
+				_putchar('0' + 0);
+				_putchar('0' + j);
+			}
+			else
+				_putchar('0' + j);
+			_putchar('\n');
+		}
+	}
 }
