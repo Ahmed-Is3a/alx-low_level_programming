ded33da827740667c64783b40b1b752099dd07a8
ahmed <ahmedissaten@gmail.com>
2023-09-15 06:14:28 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - print upper case letters.
+ *
+ * @c: char
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+char _putchar(char);
+int _isupper(int c);
+#endif
