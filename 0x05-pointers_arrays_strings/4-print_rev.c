f2905b08de9085ae03fd359471b4087b7f0c8e91
ahmed <ahmedissaten@gmail.com>
2023-09-18 14:49:20 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,19 @@
 #include "main.h"
+#include <string.h>
 
 /**
- *  - print upper case letters.
+ * print_rev - print upper case letters.
  *
- * @c: char
+ * @s: char
  * Return: always 0
  */
 
+void print_rev(char *s)
 {
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 }
