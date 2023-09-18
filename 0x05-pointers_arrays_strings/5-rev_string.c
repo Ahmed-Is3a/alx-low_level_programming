de393e716db5018daf1eed3f7744e61d8b9bc9bc
ahmed <ahmedissaten@gmail.com>
2023-09-18 15:02:07 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_strin - print upper case letters.
+ *
+ * @s: char
+ * Return: always 0
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char a[strlen(s)];
+
+	j = 0;
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		a[j] = s[i];
+		j++;
+	}
+	s = a;
+}
