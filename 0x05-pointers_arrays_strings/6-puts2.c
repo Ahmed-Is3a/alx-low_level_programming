7f5525c044cb65203e88fe93754433ba1e5a20f7
ahmed <ahmedissaten@gmail.com>
2023-09-18 15:32:20 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts2 - print upper case letters.
+ *
+ * @str: char
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < l; i = i + 2)
+	{
+		puts(str(i));
+	}
+}
