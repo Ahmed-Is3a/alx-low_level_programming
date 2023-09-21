5a574c288c07a0c402e88c2fdacef3b6b3ed981b
ahmed <ahmedissaten@gmail.com>
2023-09-21 13:46:38 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * rot13 -  encodes a string into 1337. leet
+ *
+ * @c: charactor
+ * Return: Always 0.
+ */
+
+char *rot13(char *c)
+{
+	char *p = c;
+
+	while (*c != '\0')
+	{
+		if (*c <= 'm' && *c >= 'a')
+		{
+			*c = *c + 13;
+		}
+		else if (*c >= 'n' && *c <= 'z')
+			*c = *c - 13;
+		if (*c >= 'N' && *c <= 'Z')
+			*c = *c + 13;
+		else if (*c <= 'M' && *c >= 'A')
+			*c = *c - 13;
+	}
+	return (p);
+}
+
+
