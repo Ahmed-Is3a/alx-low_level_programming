5fa9b9c651635af2d150414f67bd0f18683e0e97
ahmed <ahmedissaten@gmail.com>
2023-09-21 08:38:17 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * cap_string - check the code
+ *
+ * @p: pointer
+ * Return: Always 0.
+ */
+char *cap_string(char *p)
+{
+	char *b;
+
+	b = p;
+
+	while (*p != '\0')
+	{
+		if (*p == ' ' || *p == '\n' || *p == '.' || *p == '\t')
+		{
+			p++;
+			*p = toupper(*p);
+		}
+		else
+			p++;
+	}
+	return (b);
+}
+
