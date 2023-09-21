bc431b86e53fcd585013634f41e2b5e4e13601c9
ahmed <ahmedissaten@gmail.com>
2023-09-21 07:39:06 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - check the code
+ *
+ * @s1: destination
+ * @s2: source
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+
+}
+
