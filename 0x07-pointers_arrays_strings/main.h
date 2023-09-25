ab15c7e10b31ed4c770f8677d4d8285b9cfe2e96
ahmed <ahmedissaten@gmail.com>
2023-09-25 18:44:40 +0300

    main.c update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,14 +4,6 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *string_toupper(char *);
-char *cap_string(char *);
-
-char *rot13(char *c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
