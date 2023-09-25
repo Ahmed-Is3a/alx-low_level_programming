5a733fe17c85a3b140bf4d5fecdb8c92717ee046
ahmed <ahmedissaten@gmail.com>
2023-09-25 18:58:52 +0300

    charactor in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locate a charactor
+ * @s: memory area.
+ * @c: char.
+ *
+ * Return: pointer.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
