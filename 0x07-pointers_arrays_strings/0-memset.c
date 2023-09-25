81d46cf60eb621e8bf2a3a3ef21feda8fb22d82e
ahmed <ahmedissaten@gmail.com>
2023-09-25 17:29:18 +0300

    0 memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - prints buffer in hexa
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: int
+ * Return: s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	char *p = s;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+
+	return (p);
+}
