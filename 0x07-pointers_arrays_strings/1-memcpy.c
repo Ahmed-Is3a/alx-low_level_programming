e781b1117401709fee58b21e861b451bf09c9689
ahmed <ahmedissaten@gmail.com>
2023-09-25 18:51:12 +0300

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _memset - prints buffer in hexa
+ * _memcpy - prints buffer in hexa
  * @dest: memory area.
  * @src: constant byte.
  * @n: int
@@ -10,15 +10,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-    char *p = dest;
+	unsigned int i;
+	char *p = dest;
 
-    for (i = 0; i < n; i++)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
+	for (i = 0; i < n; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 
-    return (p);
+	return (p);
 }
