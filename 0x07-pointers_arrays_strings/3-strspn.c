825d97c24c4bcca77516c184e75ff2d088457eb2
ahmed <ahmedissaten@gmail.com>
2023-09-26 20:04:43 +0300

    3 strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - locate a charactor
+ * @s: memory area.
+ * @accept: char.
+ *
+ * Return: pointer.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+
+	n = 0;
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				n++;
+				continue;
+			}
+		}
+		if (*s != *accept && n > 0)
+			break;
+		s++;
+		accept++;
+	}
+
+	return (n);
+}
