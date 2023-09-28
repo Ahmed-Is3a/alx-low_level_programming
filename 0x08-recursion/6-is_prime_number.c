933789fe3597c7c0d5d060dcf56dbcd976ea7c22
ahmed <ahmedissaten@gmail.com>
2023-09-28 20:22:08 +0300

    6 is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,13 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 
+
 /**
- * - function discription
- * @s: varaible discription.
+ * prime - prime number calculation
  *
- * Return: always 0.
+ * @x: int
+ * @i: int
+ *
+ * Return: 0
+*/
+int prime(int x, int i)
+{
+	if (x <= 1)
+		return (0);
+
+	if (x == 2)
+		return (1);
+
+	if (x % i == 0)
+		return (0);
+
+	if (i * i > x)
+		return (1);
+
+	return (prime(x, i + 1));
+}
+
+/**
+ * is_prime_number - function discription
+ *
+ * @n: varaible discription.
+ *
+ * Return: 0 or 1.
  */
 int is_prime_number(int n)
 {
-	
+	return (prime(n, 2));
+
 }
