ae1bf4da484b0baeeaa6d47bd3e96b6dffaba6d6
ahmed <ahmedissaten@gmail.com>
2023-09-28 11:38:06 +0300

    4 power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,12 +3,20 @@
 #include <string.h>
 
 /**
- * - function discription
- * @s: varaible discription.
- *
+ * _pow_recursion - function discription
+ * @x: varaible discription.
+ * @y: int.
  * Return: always 0.
  */
+
 int _pow_recursion(int x, int y)
 {
-	
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+
 }
