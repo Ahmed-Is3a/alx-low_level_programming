a2788ae2ba1c9954b6b6c1e8890744cf301151df
ahmed <ahmedissaten@gmail.com>
2023-09-28 11:19:20 +0300

    3 factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,18 @@
 #include <string.h>
 
 /**
- * - function discription
- * @s: varaible discription.
+ * factorial - function discription
+ * @n: varaible discription.
  *
  * Return: always 0.
  */
 int factorial(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+
 }
