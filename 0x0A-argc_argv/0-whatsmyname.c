1400073b0b03b7ad07e16ed95a2046415019b17a
ahmed <ahmedissaten@gmail.com>
2023-10-02 12:01:16 +0200

    0 bug fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,15 @@
-/**
- * Write a program that prints its name, followed by a new line.
- * 
- * If you rename the program, it will print the new name, without having to compile it again
- * You should not remove the path before the name of the program
- * 
-*/
-
 #include "main.h"
 
 /**
  * main - check the code
  *
+ * @argc: arg
+ * @argv: arg
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	
+
 	return (0);
 }
