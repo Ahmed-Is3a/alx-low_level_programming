8ce4238847dd6cbc48b20d8153439322e6ca84b0
ahmed <ahmedissaten@gmail.com>
2023-10-02 12:29:43 +0200

    2- betty fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,21 +1,21 @@
-/**
- * Write a program that prints all arguments it receives.
- *
- * All arguments should be printed, including the first one
- * Only print one argument per line, ending with a new line
- */
-
 #include "main.h"
 
 /**
  * main - check the code
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Write a program that prints all arguments it receives.
+ *
+ * All arguments should be printed, including the first one
+ * Only print one argument per line, ending with a new line
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	
 
 	for (i = 0; i < argc; i++)
 	{
