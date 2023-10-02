aa710d76f73a55f4873a7b7395f96fdf9c68d854
ahmed <ahmedissaten@gmail.com>
2023-10-02 09:22:20 +0200

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+/**
+ * Write a program that multiplies two numbers.
+ * 
+ * Your program should print the result of the multiplication, followed by a new line
+ * You can assume that the two numbers and result of the multiplication can be stored in an integer
+ * If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1
+ * 
+*/
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv)
+{
+	int sum = 0;
+
+	sum = argv[1] * argv[2];
+	printf("%d", sum);
+	
+	return (0);
+}
