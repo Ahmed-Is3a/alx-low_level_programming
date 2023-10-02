2cd47f0b6b82dd38985fffef62858c7aed930e65
ahmed <ahmedissaten@gmail.com>
2023-10-02 12:34:15 +0200

    3- betty fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,18 @@
-/**
- * Write a program that multiplies two numbers.
- * 
- * Your program should print the result of the multiplication, followed by a new line
- * You can assume that the two numbers and result of the multiplication can be stored in an integer
- * If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1
- * 
-*/
 #include "main.h"
 #include <stdlib.h>
 
 /**
  * main - check the code
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Write a program that multiplies two numbers.
+ *
+ * Your program should print the result, followed by a new line
+ * You can assume that the two numbers and result can be stored in an integer
+ * If the program does not receive two arguments,
+ * your program should print Error, followed by a new line, and return 1
  *
  * Return: Always 0.
  */
@@ -29,6 +31,6 @@ int main(int argc, char *argv[])
 	b = atoi(argv[2]);
 	sum = a * b;
 	printf("%d\n", sum);
-	
+
 	return (0);
 }
