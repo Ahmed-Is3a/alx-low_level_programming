5aeccd6f9c63da01ca517abda630ef41ef59919c
ahmed <ahmedissaten@gmail.com>
2023-10-02 12:23:51 +0200

    1 unused arg fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,20 @@
-/**
- * Write a program that prints the number of arguments passed into it.
- * 
- * Your program should print a number, followed by a new line
-*/
+
 
 #include "main.h"
 
 /**
  * main - check the code
+ * @argc: arg
+ * @argv: arg
+ *
+ * Write a program that prints the number of arguments passed into it.
+ * Your program should print a number, followed by a new line
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
+	(void)argv;
 	printf("%d\n", argc);
 
 	return (0);
