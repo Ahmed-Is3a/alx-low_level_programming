b675bc874f98fda5271bd28ab5ad134dba901862
ahmed <ahmedissaten@gmail.com>
2023-10-03 20:26:48 +0200

    2-str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,5 +12,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int i;
+	char *str1 = (char *)malloc(sizeof(char) * 200);
+	char *ptr = str1;
+
+	if (str1 == NULL)
+		return (NULL);
+	if (s1 != NULL)
+	{
+		for (i = 0; *s1 != '\0'; i++)
+		{
+			*str1 = *s1;
+			s1++;
+			str1++;
+		}
+	}
+	if (s2 != NULL)
+	{
+		for (i = 0; *s2 != '\0'; i++)
+		{
+			*str1 = *s2;
+			s2++;
+			str1++;
+		}
+	}
+
+	*str1 = '\0';
+	return (ptr);
+
 
 }
