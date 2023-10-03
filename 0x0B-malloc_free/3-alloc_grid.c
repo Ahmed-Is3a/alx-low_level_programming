f25efecfbe5dddff8d86425fc22c95a2af90f684
ahmed <ahmedissaten@gmail.com>
2023-10-03 21:00:11 +0200

    30alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,5 +12,25 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **a = (int **)malloc(sizeof(int *) * (width));
+	int **ptr = a;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+		a[i] = (int *)malloc(height * sizeof(int));
+
+	/*Note that arr[i][j] is same as *(*(arr+i)+j) */
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+			a[i][j] = 0; /*OR *(*(arr+i)+j) = 0 */
+	}
+
+
+	return (ptr);
 }
