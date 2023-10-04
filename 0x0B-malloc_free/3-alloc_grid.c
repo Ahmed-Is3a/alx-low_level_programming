d9f0839db35562059bcf006035aecaf80f3048f2
ahmed <ahmedissaten@gmail.com>
2023-10-04 09:14:52 +0200

    3-alloc fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - check the code for ALX School students.
+ * alloc_grid - check the code.
  *
  * @width: width
  * @height: height
@@ -20,17 +20,17 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
+	if (a == NULL)
+		return (NULL);
 
 	for (i = 0; i < width; i++)
 		a[i] = (int *)malloc(height * sizeof(int));
 
-	/*Note that arr[i][j] is same as *(*(arr+i)+j) */
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
-			a[i][j] = 0; /*OR *(*(arr+i)+j) = 0 */
+			a[i][j] = 0;
 	}
-
 
 	return (ptr);
 }
