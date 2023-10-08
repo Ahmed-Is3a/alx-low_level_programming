4c115a5c09fd298940d5d2d0673f19e2c31bde45
ahmed <ahmedissaten@gmail.com>
2023-10-08 12:04:54 +0200

    100 fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	for (i = 0; i < old_size; i++)
+	for (i = 0; i < size; i++)
 	{
 		((char *)p)[i] = ((char *)ptr)[i];
 	}
