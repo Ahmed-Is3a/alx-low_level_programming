e6419980d1efd068aa3dde4a174e032362f003f6
ahmed <ahmedissaten@gmail.com>
2023-10-10 19:11:50 +0200

    dog.h update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG
 #define DOG
 
+
 /**
  * struct dog - dog struct
  * @name: name
@@ -13,5 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
