a69554b8f9a08079055f5fffe3e9822d731e0071
ahmed <ahmedissaten@gmail.com>
2023-10-10 13:19:18 +0200

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - rints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	printf(__FILE__);
+	putchar('\n');
+
+	return (0);
+}
