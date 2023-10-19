9127270b2853f60df4842d4c26363eb3623aae02
ahmed <ahmedissaten@gmail.com>
2023-10-19 22:42:40 +0200

    0-print list bug fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include "lists.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_list - prints all the elements of a linked list
@@ -11,13 +24,32 @@
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
+	char *tmp;
+	int i;
 
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)\n");
+		{
+			tmp = "[0] (nil)\n";
+			for (i = 0; tmp[i] != '\0'; i++)
+			{
+				_putchar(tmp[i]);
+			}
+		}
 		else
-			printf("[%u] %s\n", h->len, h->str);
+		{
+			_putchar('[');
+			_putchar('0' + h->len);
+			_putchar(']');
+			_putchar(' ');
+			tmp = h->str;
+			for (i = 0; tmp[i] != '\0'; i++)
+			{
+				_putchar(tmp[i]);
+			}
+			_putchar('\n');
+		}
 		h = h->next;
 		s++;
 	}
