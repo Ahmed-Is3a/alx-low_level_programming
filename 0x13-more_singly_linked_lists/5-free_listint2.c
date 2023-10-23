862e88c4aea72d82f6950a8908822436d196a5e1
ahmed <ahmedissaten@gmail.com>
2023-10-23 23:13:39 +0200

    init 5- free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list.
+ * The function sets the head to NULL
+ *
+ * @head: head
+ *
+*/
+void free_listint2(listint_t **head)
+{
+
+}
