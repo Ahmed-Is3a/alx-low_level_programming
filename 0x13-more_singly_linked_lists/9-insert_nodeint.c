824ab57344340202dc9965f03be179b403e8e231
ahmed <ahmedissaten@gmail.com>
2023-10-23 23:28:15 +0200

    init 9- insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ *
+ * @head: head
+ * @idx: idx
+ * @n: n
+ *
+ * Return: address of the new node,
+ *	or NULL if it failed
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+}
