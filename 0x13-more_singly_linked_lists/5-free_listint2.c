726847f09bf159d1f19747fc7a12ee7ec68c27a1
ahmed <ahmedissaten@gmail.com>
2023-10-24 16:25:35 +0200

    5 & 6

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,12 +2,24 @@
 
 /**
  * free_listint2 -  frees a listint_t list.
- * The function sets the head to NULL
+ *	The function sets the head to NULL
  *
  * @head: head
- *
-*/
+ */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	*head = NULL;
 }
+
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,5 +10,15 @@
 */
 int pop_listint(listint_t **head)
 {
+	listint_t *new;
+	int n;
+	
+	if (*head == NULL)
+		return (0);
 
+	new = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = new;
+	return (n);
 }
