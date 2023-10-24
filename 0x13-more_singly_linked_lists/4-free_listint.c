bf6c14612f14a170e406d5eeb540a08bbf42dfe4
ahmed <ahmedissaten@gmail.com>
2023-10-24 16:29:22 +0200

    4 free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,8 +4,17 @@
  * free_listint - Write a function that frees a listint_t list.
  *
  * @head: head
-*/
+ */
 void free_listint(listint_t *head)
 {
+	listint_t *current_node = head;
+	
+	while (current_node != NULL)
+	{
+		listint_t *next = current_node->next;
+		free(current_node);
+
+		current_node = next;
+	}
 
 }
