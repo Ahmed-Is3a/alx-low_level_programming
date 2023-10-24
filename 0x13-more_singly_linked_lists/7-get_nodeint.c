1b3707372f39bcca01dad1f68a1ac0593784dff9
ahmed <ahmedissaten@gmail.com>
2023-10-24 17:09:47 +0200

    7 get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,8 +7,22 @@
  * @index: the index of the node, starting at 0
  *
  * Return: the nth node
-*/
+ *	NULL if node do not exist
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *tmp = head;
 
+	if (tmp == NULL)
+		return (NULL);
+
+	while (i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	return (tmp);
 }
+
