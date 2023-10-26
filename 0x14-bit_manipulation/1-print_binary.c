b7ae78ee38b3116cbe960b01b19b575c6cca0a86
ahmed <ahmedissaten@gmail.com>
2023-10-26 22:35:51 +0200

    1prrint fixxxx

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * power - base ** power
+ *
+ * @base: base
+ * @power: power
+ *
+ * Return: power
+ */
+unsigned long int power(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary -  prints the binary representation of a number
+ *
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divider;
+	unsigned long int value;
+	char flag;
+
+	flag = 0;
+	divider = power(2, 7);
+	while (divider != 0)
+	{
+		value = n & divider;
+		if (value == divider)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divider == 1)
+		{
+			_putchar('0');
+		}
+		divider >>= 1;
+	}
+}
