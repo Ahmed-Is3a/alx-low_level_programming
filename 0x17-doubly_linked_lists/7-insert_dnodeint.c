92b8f429173cebf4955462720e7e25639b9a2b7c
ahmed <ahmedissaten@gmail.com>
2023-12-08 16:54:14 +0200

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,35 +10,43 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new_node, *next_node;
-	unsigned int i = 1;
+	dlistint_t *current, *new_node;
+	unsigned int i = 0;
 
 	current = *h;
-	next_node = (*h)->next;
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Traverse the list */
-	while (current != NULL && i < idx)
-	{
-		current = current->next;
-		next_node = next_node->next;
-		i++;
-	}
-
+	/* initialize the new node*/
 	new_node->n = n;
-    /* Check if the index is valid */
-	if (i == idx && current != NULL)
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	/* If the new node is to be inserted at the beginning */
+	if (idx == 0)
 	{
-		new_node->next = next_node;
-		new_node->prev = current;
-		current->next = new_node;
-		next_node->prev = new_node;
+		new_node->next = current;
+		if (current != NULL)
+			current->prev = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 
-	/* Node at the specified index does not exist */
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
+
+    /* Check if the index is valid */
+	if (i == idx - 1 && current != NULL)
+	{
+		new_node->next = current->next;
+		new_node->prev = current;
+		if (current->next != NULL)
+			current->next->prev = new_node;
+
+		current->next = new_node;
+		return (new_node);
+	}
+	free(new_node);
 	return (NULL);
 }
