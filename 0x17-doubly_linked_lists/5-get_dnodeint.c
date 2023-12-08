8464e9e99e8161eb0ae21a94d234cc93d1f8e996
ahmed <ahmedissaten@gmail.com>
2023-12-08 10:51:05 +0200

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - add node at index,
+ * @head: head
+ * @index: the index
+ *
+ * Return: node at index
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *tmp;
+
+	if (head == NULL)
+		return (NULL);
+
+	tmp = head;
+	for (i = 0; i < index; i++)
+	{
+		tmp = tmp->next;
+	}
+
+	return (tmp);
+}
