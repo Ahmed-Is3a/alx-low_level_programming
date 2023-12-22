56c28c2ddfff93febd87abd8a956f88c1eed0d1f
ahmed <ahmedissaten@gmail.com>
2023-12-22 22:53:50 +0200

    0-hash_table_create.c fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,9 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (h_table == NULL)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = calloc(h_table->size, sizeof(hash_node_t));
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (h_table->array == NULL)
+		return (NULL);
 
-	for (i = 0; i < h_table->size; i++)
+	for (i = 0; i < size; i++)
 		h_table->array[i] = NULL;
 
 	return (h_table);
